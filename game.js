d71e2ed7b2f1b41555016f3a289555c0c29cfc30
8bithemant <mailhemantjoshi@gmail.com>
2020-06-11 17:21:26 +0530

    added backgroun

---
+++ game.js
@@ -0,0 +1,63 @@
+const cvs = document.getElementById("bird");
+
+const ctx = cvs.getContext("2d")
+
+
+let frames = 0;
+
+const sprite = new Image()
+sprite.src = "img/sprite.png"
+
+const bg={
+    sX:0,
+    sY:0,
+    w:275,
+    h:226,
+    x:0,
+    y:cvs.height - 226,
+
+    draw: function(){
+        
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+const fg = {
+    sX: 276,
+    sY: 0,
+    w:224,
+    h:112,
+    x:0,
+    y: cvs.height - 112,
+
+    draw :function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+
+    }
+}
+
+function draw(){
+    ctx.fillStyle = '#70c5ce'
+    ctx.fillRect(0,0,cvs.width ,cvs.height)
+
+    bg.draw();
+    fg.draw();
+}
+
+function update(){
+
+}
+
+function loop(){
+    update()
+    draw()
+    frames++
+
+    requestAnimationFrame(loop)
+}
+
+loop()
